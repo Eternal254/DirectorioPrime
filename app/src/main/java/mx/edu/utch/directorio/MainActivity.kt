8eae1dac76e2c634f81522ac0e4e6180bf026cf2
Isaac.P <isaacparraaguirre381@gmail.com>
2024-03-01 02:41:29 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/utch/directorio/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.contactdirectory
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import mx.edu.utch.directorio.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.listView)
+
+        val contacts = listOf(
+            "Juan: 1234567890",
+            "María: 9876543210",
+            "Carlos: 5555555555"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contacts)
+        listView.adapter = adapter
+    }
+}
